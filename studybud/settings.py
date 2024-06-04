eed8879a62dc861618266be12c476909171bb56c
Om Uskaikar <117098130+omuskaikar@users.noreply.github.com>
2024-06-04 22:00:18 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'npjGNKSVdiXSjytKFMGYIVulRaLFlMmD',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '56837',
     }
 }
 
